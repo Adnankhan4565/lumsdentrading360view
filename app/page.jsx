9f48149994d8011a34f2b36a2bb4f82a88f5b141
Adnan khan <IGNITION420@GMAIL.COM>
2024-07-10 02:52:28 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 mt-32">
-    <h1 className="text-2xl font-bold">Groove Street 608 360 view</h1>
+    <h1 className="text-2xl font-bold">Groove Street 608 House Construction Virtual Tour</h1>
     <iframe
   width="100%"
   height={840}
